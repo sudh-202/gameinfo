26450d380549b7424d0fa5cb249c90c8199aec5b
DigitallyNext <87563365+sudh-202@users.noreply.github.com>
2024-12-06 11:38:20 +0530

    the new fix

---
+++ components/GameGrid.tsx
@@ -0,0 +1,23 @@
+import { useGameStore } from '../lib/store'
+import { GameCard } from './ui/GameCard'
+
+export function GameGrid() {
+  const filteredGames = useGameStore((state) => state.filteredGames)
+
+  return (
+    <div className="grid grid-cols-1 gap-6 p-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {filteredGames.map((game) => (
+        <GameCard
+          key={game.id}
+          title={game.title}
+          description={game.description}
+          price={game.price}
+          platform={game.platform}
+          imageUrl={game.imageUrl}
+          crackStatus={game.crackStatus}
+          releaseDate={game.releaseDate}
+        />
+      ))}
+    </div>
+  )
+}
